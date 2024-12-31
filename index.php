642a5a08b5e0c4236a9474ccd45b317fc76ce4b7
dj <dj.moh3n@gmail.com>
2024-12-31 22:40:03 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,100 @@
+
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+	<head>
+<!-- Event snippet for Page view conversion page --> <script> gtag('event', 'conversion', { 'send_to': 'AW-16753432844/5hiMCI7uweIZEIyy1LQ-', 'value': 1.0, 'currency': 'TRY' }); </script>
+		<meta charset="UTF-8">
+
+		<title>خرید وی پی ان</title>
+
+		<meta name="description" content="خرید وی پی ان">
+		<meta name="author" content="وی پی ان">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+
+	</head>
+
+	<body>
+<a href="/pay">
+		<p style="color:silver;">صفحه را لمس کنید 
+		&#128071;
+		<br>
+		هدایت به خرید وی پی ان</p>
+
+		<div class="animation" data-animation-step="1">
+			<div class="device">
+				<div class="phone-home-button"></div>
+				<div class="tablet-home-button"></div>
+				<div class="screen-stand"><div class="leg"></div><div class="foot"></div></div>
+				<div class="display" dir="rtl">
+					<div class="slide1"><div>یک VPN بگیر<br>برای <em>کامپیوتر</em><br><img width="70px" src="hand.gif"></div></div>
+					<div class="slide2"><div>یک VPN بگیر<br>برای <em>تبلت</em><br><img width="70px" src="hand.gif"></div></div>
+					<div class="slide3"><div>یک VPN بگیر<br>برای <em>موبایل</em><br><img width="70px" src="hand.gif"></div></div>
+				</div>
+			</div>
+		</div>
+
+		<script>
+
+			// this thing is js driven on slid.es so I didn't bother switching out for css animations
+			var duration = 7000, steps = 3, step = 1;
+
+			setInterval( function() {
+				document.querySelector( '.animation' ).setAttribute( 'data-animation-step', step = ++step > steps ? 1 : step );
+			}, duration / steps );
+
+		</script>
+
+		<!-- Third party scripts and sharing UI -->
+
+		<!--div class="credits">
+			<a href="">Open Your Panel</a>
+			<a href="">Follow @MyVDS</a>
+			<a href="">Tweet this</a>
+		</div-->
+
+		<style type="text/css" media="screen">
+			.credits {
+				position: absolute;
+				right: 30px;
+				bottom: 30px;
+				font-size: 15px;
+				color: #fff;
+			}
+
+			.credits>* {
+				vertical-align: middle;
+				margin-left: 15px;
+			}
+
+			.credits a {
+				padding: 4px 6px;
+				color: #333;
+				border: 2px solid #aaa;
+				border-radius: 2px;
+				text-decoration: none;
+			}
+
+			.credits a:hover {
+				border-color: #666;
+				color: #000;
+			}
+		</style>
+</a>
+
+
+
+
+
+<hr>
+<h1>خرید وی‌پی‌ان با سرعت و امنیت بالا ..</h1><h3>خرید وی‌پی‌ان با سرعت و امنیت بالا</h3><p>آیا تا به حال با مشکل باز کردن سایت‌های مسدود شده مواجه شده‌اید؟ آیا به دنبال وی‌پی‌ان پرسرعت و ایمن هستید که به شما اجازه دهد بدون محدودیت به دنیای وب دسترسی داشته باشید؟ ما اینجاییم تا شما را با بهترین وی‌پی‌ان آشنا کنیم: راهکاری برای باز کردن تمامی سایت‌های مسدود شده در هر شرایطی! با خرید وی‌پی‌ان مناسب برای دستگاه‌های اندروید، آیفون، ویندوز، مک و لینوکس، از اینترنت بی‌وقفه و بدون اختلال لذت ببرید.</p><h3>وی‌پی‌ان پرسرعت: اتصال پایدار و سریع</h3><p>وی‌پی‌ان‌هایی که ما ارائه می‌دهیم دارای قابلیت کارکرد در همه شرایط هستند. بدون قطع شدن و با سرعت بالا، می‌توانید به محتوای دلخواه خود دسترسی پیدا کنید. ویژگی‌های منحصر به فرد وی‌پی‌ان ما عبارتند از:</p><p>- <strong>کارکرد در تمامی شرایط:</strong> هیچ محدودیتی بین شما و اینترنت باقی نمی‌ماند.</p><p>- <strong>سازگار با تمامی سیستم‌ها:</strong> از وی‌پی‌ان اندروید، آیفون، ویندوز، مک و لینوکس لذت ببرید.</p><p>- <strong>بدون قطعی و پایدار:</strong> اتصال شما دچار وقفه نمی‌شود، حتی در اوج ترافیک اینترنتی.</p><h3>مزایای خرید وی‌پی‌ان مرکزی پرسرعت</h3><p>با خرید وی‌پی‌ان مرکزی، علاوه بر سرعت بی‌نظیر، از امنیت بالایی نیز بهره‌مند خواهید شد. تیم ما سعی داشته است تا با استفاده از پروتکل‌های ایمنی پیشرفته، اطلاعات شما را محافظت کند.</p><p>- <strong>پرسرعت و ایمن:</strong> هرگز نگران کندی اتصال یا افشای اطلاعات شخصی خود نباشید.</p><p>- <strong>تانل با حجم نامحدود:</strong> تمام اینترنت را با حجمی نامحدود و بدون هیچ گونه محدودیت تجربه کنید.</p><h3>تجربه عالی با وی‌پی‌ان در همه دستگاه‌ها</h3><p>فرقی نمی‌کند از کدام دستگاه استفاده می‌کنید، وی‌پی‌ان ما با همه سیستم‌ها سازگار است. در اینجا تعدادی از دستگاه‌هایی که پشتیبانی می‌شوند، ذکر شده‌اند:</p><p>- <strong>وی‌پی‌ان اندروید:</strong> دسترسی به تمامی اپلیکیشن‌ها و سایت‌ها بدون نگرانی.</p><p>- <strong>وی‌پی‌ان آیفون و آی.او.اس:</strong> آزادی استفاده از اینترنت در دستگاه‌های اپل.</p><p>- <strong>وی‌پی‌ان ویندوز و مک:</strong> بهترین گزینه برای کاربران دسکتاپ و لپ‌تاپ.</p><p>- <strong>وی‌پی‌ان لینوکس:</strong> انتخابی عالی برای حرفه ای‌ها و کاربران تخصصی.</p><p>- <strong>وی‌پی‌ان سامسونگ و هواوی:</strong> مخصوص دستگاه‌های گلکسی و برند معروف هواوی.</p><h3>چرا وی‌پی‌ان پولی؟ چرا اکانت وی‌پی‌ان بخریم؟</h3><p>شاید بپرسید چرا باید وی‌پی‌ان پولی را انتخاب کنیم؟ پاسخ ساده است: سرعت، امنیت و پشتیبانی! اکانت وی‌پی‌ان ما اطمینان حاصل می‌کند که به بهترین خدمات دسترسی دارید:</p><p>- <strong>بدون تبلیغات آزاردهنده:</strong> با خرید ویتوری از تبلیغات پشت سر هم رهایی یابید.</p><p>- <strong>پشتیبانی 24/7:</strong> تیم ما همیشه آماده کمک به شماست.</p><p>- <strong>قیمت مناسب و وی‌پی‌ان ارزان:</strong> بهترین کیفیت را با قیمتی منصفانه تجربه کنید.</p><h3>چگونه وی‌پی‌ان دلخواه خود را خریداری کنیم؟</h3><p>با ما همراه باشید و برای خرید وی‌پی‌ان با ما تماس بگیرید یا به سادگی از طریق سایت ما اقدام به خرید کنید. تیم پشتیبانی ما در تمامی مراحل خرید همراه شما خواهد بود تا بهترین تجربه اینترنتی را در اختیار داشته باشید.</p><h4>آیا آماده‌اید که اینترنت خود را بدون مرز تجربه کنید؟</h4><p>از خرید وی‌پی‌ان پرسرعت و ایمن لذت ببرید. هم‌اکنون اقدام کنید و به دنیای آرام و سریع اینترنت بدون محدودیت قدم بگذارید.
+<center>
+<br><a href="/pay" rel="" target="">همین حالا اقدام کنید</a><br>
+</center>
+و از سرعت، امنیت و آزادی آنلاین بهره‌مند شوید!</p>
+
+	</body>
+	<!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16753432844"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16753432844'); </script>
+</html>
+
